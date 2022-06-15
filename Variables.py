a9c63fc7c3e6ec662e1ce0b47169a0ccd116f646
Zosoutsav <88942040+zosoutsav@users.noreply.github.com>
2022-06-15 14:47:32 +0545

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,9 @@
+# This is how you declare a variable in python
+studentCount = 1000
+one, two = 33, 44
+studentName = 'julia'
+studentGpa = 2.9
+courseName = 'computer programming and analysis'
+# it is suggested to use camelcase while naming the variable
+# while creating a variable the name should be enough to describe what the purpose of variable
+# It's a good practice of a good programmer
