87aa7a5d34b1ef8dce23535b9002122fcfe6dfc3
Zosoutsav <88942040+zosoutsav@users.noreply.github.com>
2022-06-15 14:50:15 +0545

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,9 +1,10 @@
 # This is how you declare a variable in python
-studentCount = 1000
+studentCount = 1000 
 one, two = 33, 44
 studentName = 'julia'
 studentGpa = 2.9
 courseName = 'computer programming and analysis'
+isPresent = True #python is a case sensitive language so if you write true instead of True the variable is going to get different value
 # it is suggested to use camelcase while naming the variable
 # while creating a variable the name should be enough to describe what the purpose of variable
 # It's a good practice of a good programmer
